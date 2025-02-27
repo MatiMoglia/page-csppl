315a4ba460f8626bcb082762b38ca84906dc47ba
Matias Moglia <matimoglia951@gmail.com>
2025-02-27 15:14:25 -0300

    creation of service and coolplus cards

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,6 +1,8 @@
 <template>
     <Navbar/>
     <Slider/>
+    <ServiciosCard/>
+    <CooplusCard/>
     <Novedades/>
   </template>
   
@@ -8,12 +10,15 @@
   import Navbar from "../components/Navbar.vue";
   import Slider from "../components/Slider.vue";
   import Novedades from "../components/NovedadesGrid.vue";
-  
+  import ServiciosCard from "../components/ServiciosCard.vue";
+  import CooplusCard from "@/components/CooplusCard.vue";
   export default {
     components: {
       Navbar,
       Slider,
       Novedades,
+      ServiciosCard,
+      CooplusCard,
     },
   };
   </script>
