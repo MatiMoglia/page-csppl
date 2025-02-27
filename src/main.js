2aaf9b8ebc6b768fe0a6fde688a666c49851934c
Matias Moglia <matimoglia951@gmail.com>
2025-02-26 21:06:50 -0300

    creating slider in Inicio view

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/global.css';
-
+import 'animate.css';
 const app = createApp(App);
 
 app.use(store);
--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,59 +1,16 @@
 <template>
-    <NavbarComponent/>
-    <div class="container">
-        <section class="servicios">
-            <h3>Nuestros Servicios</h3>
-            <ul>
-                <li v-for="servicio in servicios" :key="servicio.nombre">
-                    <h4>{{ servicio.nombre }}</h4>
-                    <p>{{ servicio.descripcion }}</p>
-                </li>
-            </ul>
-        </section>
-    </div>
-</template>
-
-<script>
-import NavbarComponent from "../components/Navbar.vue";
-export default {
+    <Navbar />
+    <Slider />
+  </template>
+  
+  <script>
+  import Navbar from "../components/Navbar.vue";
+  import Slider from "../components/Slider.vue";
+  
+  export default {
     components: {
-        NavbarComponent
+      Navbar,
+      Slider,
     },
-    data() {
-        return {
-            servicios: [
-                { nombre: "Energía Eléctrica", descripcion: "Suministro de energía eléctrica." },
-                { nombre: "Agua potable", descripcion: "Distribución de agua limpia y segura." },
-                { nombre: "Telecomunicaciones", descripcion: "Conectividad rápida y confiable." },
-                { nombre: "Alumbrado Público", descripcion: "Mantenimiento de las luminarias y de las líneas." },
-                { nombre: "Servicios Sociales", descripcion: "Brindamos la posibilidad de que puedas proteger a tu familia." },
-
-            ]
-        };
-    }
-};
-</script>
-
-<style>
-.home {
-    text-align: center;
-    padding: 20px;
-}
-
-.servicios {
-    margin-top: 20px;
-}
-
-.servicios ul {
-    list-style: none;
-    padding: 0;
-}
-
-.servicios li {
-    background: #f4f4f4;
-    margin: 10px auto;
-    padding: 15px;
-    width: 50%;
-    border-radius: 8px;
-}
-</style>
+  };
+  </script>
