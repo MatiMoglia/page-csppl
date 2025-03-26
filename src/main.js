dfcbf2e45d29d00d2ede3b715bdcfa76604b2894
Matias Moglia <matimoglia951@gmail.com>
2025-03-25 23:06:45 -0300

    Latest changes to names in the index

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import Toastify from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App);
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title || "Cooperativa de Servicios Públicos de Porteña";  
+    next();
+  });
 app.use(Toastify);
 app.use(store);
 app.use(router);
--- src/router/index.js
+++ src/router/index.js
@@ -31,71 +31,85 @@ const routes = [
     path: '/',
     name: 'Inicio',
     component: Inicio,
+    meta: { title: "Inicio | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/historia',
     name: 'Historia',
     component: Historia,
+    meta: { title: "Historia | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/mvv-objetivos',
     name: 'MVVObjetivos',
     component: MVVObjetivos,
+    meta: { title: "Objetivos | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/autoridades',
     name: 'Autoridades',
     component: Autoridades,
+    meta: { title: "Autoridades | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/cooplus',
     name: 'CooplusView',
     component: CooplusView,
+    meta: { title: "COOPLUS+ | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/energia',
     name: 'EnergiaView',
     component: EnergiaView,
+    meta: { title: "Energía Eléctrica | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/agua',
     name: 'AguaView',
     component: AguaView,
+    meta: { title: "Agua Potable | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/telecomunicaciones',
     name: 'TelecomunicacionesView',
     component: InternetView,
+    meta: { title: "Internet | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/telefono',
     name: 'Telefonia',
     component: Telefonia,
+    meta: { title: "Energía Eléctrica | Cooperativa de Servicios Públicos de Porteña" } 
   },
   {
     path: '/servicios-sociales',
     name: 'ServiciosSocialesView',
     component: ServiciosSocialesView,
+    meta: { title: "Servicios Sociales | Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: '/contacto',
     name: 'ContactoView',
     component: ContactoView,
+    meta: { title: "Contacto | Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: '/detalle-sepelio',
     name: 'DetalleSepelio',
     component: detalleSepelio,
+    meta: { title: "Sepelio | Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: '/detalle-banco',
     name: 'DetalleBanco',
     component: detalleBanco,
+    meta: { title: "Banco de Sangre| Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: '/detalle-traslado',
     name: 'DetalleTraslado',
     component: detalleTraslado,
+    meta: { title: "Traslado Social | Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: '/registro',
@@ -105,6 +119,7 @@ const routes = [
       hideNavbar: true,
       hideFooter: true,
       hideSpam: true,
+      title: "Registro | Cooperativa de Servicios Públicos de Porteña"
     }
   },
   {
@@ -115,12 +130,14 @@ const routes = [
       hideNavbar: true,
       hideFooter: true,
       hideSpam: true,
+      title: "Login | Cooperativa de Servicios Públicos de Porteña"
     }
   },
   {
     path: '/administracion',
     name: 'AdministracionView',
     component: AdministracionView,
+    meta: { title: "Panel de Administracion | Cooperativa de Servicios Públicos de Porteña" },
     beforeEnter: (to, from, next) => {
       if (store.state.auth.user && store.state.auth.user.isAdmin) {
         next();
@@ -133,21 +150,26 @@ const routes = [
     path: '/novedades',
     name: 'NovedadesView',
     component: NovedadesView,
+    meta: { title: "Novedades | Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: "/novedad/:id",
     name: "NovedadDetalle",
+    meta: { title: "Detalle de la Novedad | Cooperativa de Servicios Públicos de Porteña" },
     component: () => import("@/components/NovedadDetalle.vue"),
+
   },
   {
     path: '/reclamos',
     name: 'ReclamosView',
     component: ReclamosView,
+    meta: { title: "Reclamos | Cooperativa de Servicios Públicos de Porteña" }
   },
   {
     path: '/perfil',
     name: 'PerfilView',
     component: PerfilView,
+    meta: { title: "Mi Perfil | Cooperativa de Servicios Públicos de Porteña" },
     beforeEnter: (to, from, next) => {
       if (store.state.auth.user) {
         next();
@@ -160,16 +182,19 @@ const routes = [
     path: '/testVelocidad',
     name: 'testInt',
     component: testInt,
+    meta: { title: "Test de Velocidad | Cooperativa de Servicios Públicos de Porteña" }  //, beforeEnter: (to, from, next) => { if (store.state.auth.user) { next(); } else { next('/login'); } }
   },
   {
     path: '/facturaDigital',
     name: 'FacturaDigital',
     component: facturaDigital,
+    meta: { title: "Factura Digital | Cooperativa de Servicios Públicos de Porteña" }  //, beforeEnter: (to, from, next) => { if (store.state.auth.user) { next(); } else { next('/login'); } }
   },
   {
     path: '/necrologicos',
     name: 'NecrologicosView',
     component: NecrologicosView,
+    meta: { title: "Necrología | Cooperativa de Servicios Públicos de Porteña" }
   }
   
 ]
